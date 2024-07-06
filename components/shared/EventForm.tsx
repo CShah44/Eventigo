8283b73a3e4b26238522d405f83ba394427246c2
Chaitya Shah <chaityashah36@gmail.com>
2024-07-06 14:23:03 +0530

    Will need to fix webhook bug

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,10 @@
+type EventFormProps = {
+  userId: string;
+  type: "Create" | "Update";
+};
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+  return <div>EventForm</div>;
+};
+
+export default EventForm;
