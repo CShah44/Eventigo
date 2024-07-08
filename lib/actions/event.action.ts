d604c3a522286b14a30f6f2493b921b22e7189dd
Chaitya Shah <chaityashah36@gmail.com>
2024-07-08 12:35:05 +0530

    Trying Form and probab webhook fix

--- lib/actions/event.action.ts
+++ lib/actions/event.action.ts
@@ -3,7 +3,18 @@ import { connectToDatabase } from "../database";
 import Event from "../database/models/event.model";
 import User from "../database/models/user.model";
 import { handleError } from "../utils";
-import { CreateEventParams } from "@/types";
+import { CreateEventParams, GetRelatedEventsByCategoryParams } from "@/types";
+import Category from "../database/models/category.model";
+
+const populateEvent = (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
@@ -20,6 +31,51 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     revalidatePath(path);
 
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function getEventById(eventId: string) {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) throw new Error("Event not found");
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function getRelatedEventsByCategory({
+  categoryId,
+  eventId,
+  limit = 3,
+  page = 1,
+}: GetRelatedEventsByCategoryParams) {
+  try {
+    await connectToDatabase();
+
+    const skipAmount = (Number(page) - 1) * limit;
+    const conditions = {
+      $and: [{ category: categoryId }, { _id: { $ne: eventId } }],
+    };
+
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
+
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    };
   } catch (error) {
     handleError(error);
   }
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -13,7 +13,6 @@ const UserSchema = new Schema({
   },
   username: {
     type: String,
-    unique: true,
   },
   firstName: {
     type: String,
